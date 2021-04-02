41b43fe3b87da85f0798237d0bcccffa8501e5fa
rsathianathen <rsathianathen@gmail.com>
2021-04-02 01:54:06 -0400

    starting code

---
+++ CIS195FinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CIS195FinalProject
+//
+//  Created by Ryan Sathianathen on 4/2/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
