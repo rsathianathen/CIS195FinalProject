aa5f483a3dce2d99ba01246472aad7f643bd69eb
rsathianathen <rsathianathen@gmail.com>
2021-04-02 21:37:52 -0400

    "viewcontroller"

--- CIS195FinalProject/ViewController.swift
+++ CIS195FinalProject/ViewController.swift
@@ -14,7 +14,36 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // TODO: How many sections? (Hint: we have 1 section and x rows)
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
-
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+            return  UITableViewCell()
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.performSegue(withIdentifier:"DVCTransition", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+       
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75.0
+    }
+    
 }
 
